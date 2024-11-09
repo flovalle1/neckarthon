2274df4c5899839bd2bdaa4dff1a130046516acc
Florian Martel <hello@florianmartel.de>
2024-11-09 14:26:28 +0100

    Updates

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,8 @@ import {
   CardHeader,
   CardTitle
 } from "@/components/ui/card";
+import Image from 'next/image';
 import { useEffect, useState } from "react";
-
-import { Button } from "@/components/ui/button";
 
 import {
   Tabs,
@@ -51,49 +50,24 @@ export default function Home() {
   }, []);
 
   return (
-
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid justify-items-center min-h-screen p-8 pb-10 gap-1 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Tabs defaultValue="overview" className="space-y-4">
         <TabsList>
-          <TabsTrigger value="overview">Overview</TabsTrigger>
-          <TabsTrigger value="analytics" disabled>
-            Analytics
-          </TabsTrigger>
-          <TabsTrigger value="reports" disabled>
-            Reports
-          </TabsTrigger>
-          <TabsTrigger value="notifications" disabled>
-            Notifications
-          </TabsTrigger>
+          <TabsTrigger value="overview">Nutzerübersicht</TabsTrigger>
+          <TabsTrigger value="analytics">Meinungsbild</TabsTrigger>
+          <TabsTrigger value="reports">Ideenfindung</TabsTrigger>
         </TabsList>
       </Tabs>
-      <Card>
+      <Card className="h-32">
         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-          <CardTitle className="text-sm font-medium">
-            Registrierte Nutzer
-          </CardTitle>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            className="h-4 w-4 text-muted-foreground"
-          >
-            <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
-          </svg>
+          <CardTitle className="text-sm font-medium">Registrierte Nutzer</CardTitle>
         </CardHeader>
         <CardContent>
           <div className="text-2xl font-bold">{usersCount}</div>
-          <p className="text-xs text-muted-foreground">
-            Bei WhatsApp registriert
-          </p>
-          <Button>Testbutton</Button>
+          <p className="text-xs text-muted-foreground">Bei WhatsApp registriert</p>
         </CardContent>
       </Card>
-      <div className="space-y-3">
+      <div className="space-y-3 h-96 overflow-y-scroll">
         {users.map((user) => {
           return (
             <div className="space-y-1" key={user.name}>
@@ -102,7 +76,7 @@ export default function Home() {
                   <AvatarImage src="/02.png" alt="Avatar" />
                   <AvatarFallback>OM</AvatarFallback>
                 </Avatar>
-                <div className="ml-4 space-y-1">
+                <div className="ml-4 mr-8 space-y-1">
                   <p className="text-sm font-medium leading-none">{user.name}</p>
                 </div>
               </div>
@@ -110,7 +84,14 @@ export default function Home() {
           );
         })}
       </div>
-
+      <Image
+        src="/qr-code-neckar.png"
+        alt="Fixed Image"
+        width={300}
+        height={300}
+        className="fixed top-1/2 transform -translate-y-1/2"
+        style={{ right: '180px' }}
+      />
     </div>
   );
 }
